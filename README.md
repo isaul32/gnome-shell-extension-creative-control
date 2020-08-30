7586535951d9c8367c8c0c3df2c2c0cfe51b0c4d
Sauli Anto <sauli.anto@gmail.com>
2020-08-30 15:40:28 +0300

    Add initial files

---
+++ README.md
@@ -0,0 +1,27 @@
+# Creative Sound Blaster control extension
+
+This extension is for control Creative Sound Core 3D (CA0132) based audio cards. It uses ALSA's amixer under the hood. A The extension should support following Creative products:
+- Sound Blaster Recon3D
+- Sound Blaster Recon3Di (commonly found on motherboards, and some laptops)
+- Sound Blaster Z
+- Sound Blaster ZxR
+- Sound Blaster ZxRi (commonly found on some Gigabyte motherboards)
+- Sound BlasterX AE-5
+
+The extension is only tested to work with Sound Blaster Z card.
+
+## Screenshots
+![Screenshot 1](/screenshots/1.png?raw=true "Screenshot 1")
+
+![Screenshot 2](/screenshots/2.png?raw=true "Screenshot 2")
+
+## Features
+- Output select
+  - Surround
+  - Headphone
+  - Line Out
+- Mic Boost
+  - 0 dB
+  - 10 dB
+  - 20 dB
+  - 30 dB
---
+++ extension.js
@@ -0,0 +1,117 @@
+const Gio = imports.gi.Gio;
+const GLib = imports.gi.GLib;
+const GObject = imports.gi.GObject;
+const St = imports.gi.St;
+const Atk = imports.gi.Atk;
+const Shell = imports.gi.Shell;
+
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
+const Main = imports.ui.main;
+const PanelMenu = imports.ui.panelMenu;
+const PopupMenu = imports.ui.popupMenu;
+const Slider = imports.ui.slider;
+const Util = imports.misc.util;
+
+var CreativeControl = GObject.registerClass(
+class CreativeControl extends PanelMenu.Button {
+
+    _init() {
+        super._init(0.0, `${Me.metadata.name}`, false);
+
+        this.accessible_role = Atk.Role.TOGGLE_BUTTON;
+        this._windowTracker = Shell.WindowTracker.get_default();
+ 
+        // Panel icon
+        this.actor.add_child(new St.Icon({
+            gicon: new Gio.ThemedIcon({name: 'audio-card-symbolic'}),
+            style_class: 'system-status-icon'
+        }));
+
+        // Output menu
+        this.outputMenu = new PopupMenu.PopupSubMenuMenuItem('', true);
+        this.outputMenu.label.text = 'Output';
+        this.outputMenu.icon.icon_name = 'audio-card-symbolic';
+
+        this.outputMenu.menu.addAction('Surround', this._surroundAction, 'audio-speakers-symbolic');
+        this.outputMenu.menu.addAction('Line Out', this._lineOutAction, 'audio-speakers-symbolic')
+        this.outputMenu.menu.addAction('Headphone', this._headphoneAction, 'audio-headphones-symbolic');
+
+        this.menu.addMenuItem(this.outputMenu);
+
+        // Separator
+        this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+
+        // Mic Boost menu
+        this.micBoostMenu = new PopupMenu.PopupSubMenuMenuItem('', true);
+        this.micBoostMenu.label.text = 'Mic Boost';
+        this.micBoostMenu.icon.icon_name = 'audio-input-microphone-symbolic';
+
+        this.micBoostMenu.menu.addAction('0 dB', this._micBoost0Action, 'audio-headphones-symbolic');
+        this.micBoostMenu.menu.addAction('10 dB', this._micBoost1Action, 'audio-headphones-symbolic');
+        this.micBoostMenu.menu.addAction('20 dB', this._micBoost2Action, 'audio-headphones-symbolic');
+        this.micBoostMenu.menu.addAction('30 dB', this._micBoost3Action, 'audio-headphones-symbolic');
+
+        this.menu.addMenuItem(this.micBoostMenu);
+    }
+
+    _surroundAction() {
+        log('Surround selected');
+        Util.spawnCommandLine(`amixer -c 'Creative' set 'Output Select' 'Surround'`);
+    }
+
+
+    _lineOutAction() {
+        log('Line Out selected');
+        Util.spawnCommandLine(`amixer -c 'Creative' set 'Output Select' 'Line Out'`);
+    }
+
+    _headphoneAction() {
+        log('Headphone selected');
+        Util.spawnCommandLine(`amixer -c 'Creative' set 'Output Select' 'Headphone'`);
+    }
+
+    _micBoost0Action() {
+        log('Mic Boost 0 dB');
+        Util.spawnCommandLine(`amixer -c 'Creative' set 'Mic Boost' '0 dB'`);
+    }
+
+    _micBoost1Action() {
+        log('Mic Boost 10 dB');
+        Util.spawnCommandLine(`amixer -c 'Creative' set 'Mic Boost' '10 dB'`);
+    }
+
+    _micBoost2Action() {
+        log('Mic Boost 20 dB');
+        Util.spawnCommandLine(`amixer -c 'Creative' set 'Mic Boost' '20 dB'`);
+    }
+
+    _micBoost3Action() {
+        log('Mic Boost 30 dB');
+        Util.spawnCommandLine(`amixer -c 'Creative' set 'Mic Boost' '30 dB'`);
+    }
+});
+
+class Extension {
+    constructor() {
+    }
+
+    enable() {
+        log(`enabling ${Me.metadata.name} version ${Me.metadata.version}`);
+        this.control = new CreativeControl();
+        Main.panel.addToStatusArea(`${Me.metadata.name} Control`, this.control);
+    }
+
+    disable() {
+        log(`disabling ${Me.metadata.name} version ${Me.metadata.version}`);
+        if (this.control !== null) {
+            this.control.destroy();
+            this.control = null;
+        }
+    }
+}
+
+function init() {
+    log(`initializing ${Me.metadata.name} version ${Me.metadata.version}`);
+    return new Extension();
+}
